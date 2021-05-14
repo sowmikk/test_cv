e848739d0c6b31f56131035b04bb0623bb08132c
Sowmik Kanti Deb <sowmikk032@gmail.com>
2021-05-14 22:57:54 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -136,6 +136,7 @@ inputs.forEach((input) => {
 });
 
 
+
 // ----------------------- google sheet connection ------------------------------
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxaBu4TA0iev2snejP6RdX0ulbzUIShVgiXSxBs8CJpyae1lQeMfwYecsG5obfuXze0/exec'
 const form = document.forms['google-sheet']
@@ -143,6 +144,16 @@ const form = document.forms['google-sheet']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+  // .then(response => alert("Thank you!! I will get back to you soon."))
   .catch(error => console.error('Error!', error.message))
+  document.querySelector(".backdrop").classList.add("showBackdrop");
+  document.getElementsByClassName("popup")[0].classList.add("active");
+  document.getElementById("contact_form").reset();
 })
+
+
+document.getElementById("dismiss-popup-btn").addEventListener("click", function() {
+  document.getElementsByClassName("popup")[0].classList.remove("active");
+  document.querySelector(".backdrop").classList.remove("showBackdrop");
+});
+
